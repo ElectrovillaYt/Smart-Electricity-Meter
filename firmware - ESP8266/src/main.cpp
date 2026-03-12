91c06ddb731b191044d64decf5d41a4e5da6e185
ElectrovillaYt <electrovillaofficial@gmail.com>
2026-03-12 15:08:27 +0530

    bug-fix

--- firmware - ESP8266/src/main.cpp
+++ firmware - ESP8266/src/main.cpp
@@ -6,7 +6,8 @@
 // PICO GPIO 13- ESP TX
 // =======================
 
-#define UART_FLAG 5 // (GPIO5) - D1
+#define PICO_FEEDBACK 4 // (GPIO-4) - D2
+#define BLYNK_FLAG 5    // (GPIO-5) - D1
 
 // Blynk Credentials
 #define BLYNK_TEMPLATE_ID "BLYNK_TEMPLATE_ID"
@@ -15,30 +16,52 @@
 
 #include <BlynkSimpleEsp8266.h>
 
-char ssid[] = "WiFi SSID";     // Replace with you WiFi SSID/Name (2.4Ghz band only!)
-char pass[] = "WiFi Password"; // Replace with you WiFi Password
+char ssid[] = "WiFi_SSID"; // Replace with you WiFi SSID/Name (2.4Ghz band only!)
+char pass[] = "WiFi_PASSWORD";        // Replace with you WiFi Password
+
+static float prev_unit = 0;
+static float prev_bill = 0;
+unsigned long lastReconnectAttempt = 0;
+unsigned long long syncHault = 0;
 
 char rxBuff[32];
 unsigned int idx = 0;
 
-unsigned long lastReconnectAttempt = 0;
+void FlashStatus(unsigned long durationMS = 0UL, bool status = 0)
+{
+    static bool LED_ON = false; // ACTIVE-LOW
+    static unsigned long long Flash_timer = 0;
+    if (durationMS == 0)
+    {
+        digitalWrite(LED_BUILTIN, !status); // ACTIVE-LOW
+    }
+    else
+    {
+        if (millis() - Flash_timer > durationMS)
+        {
+            Flash_timer = millis();
+            LED_ON = !LED_ON;
+            digitalWrite(LED_BUILTIN, LED_ON);
+        }
+    }
+}
 
 void setup()
 {
-    pinMode(UART_FLAG, OUTPUT);
+    pinMode(BLYNK_FLAG, OUTPUT);
+    pinMode(PICO_FEEDBACK, INPUT);
     pinMode(LED_BUILTIN, OUTPUT);
     Serial.begin(115200);
-    digitalWrite(LED_BUILTIN, 1);
+    FlashStatus(0, 0);
     delay(100);
     Blynk.begin(BLYNK_AUTH_TOKEN, ssid, pass);
 }
 
 BLYNK_CONNECTED()
 {
-    Blynk.syncVirtual(V0, V1, V2);
-
-    digitalWrite(UART_FLAG, 1);
-    digitalWrite(LED_BUILTIN, 0);
+    Blynk.syncVirtual(V2, V1, V0);
+    digitalWrite(BLYNK_FLAG, 1);
+    FlashStatus(0, 1);
 }
 
 BLYNK_WRITE(V0) // Switch 1 - V0
@@ -62,34 +85,75 @@ BLYNK_WRITE(V2) // Total Units (Kw/h) -  V2
 
 void loop()
 {
+    static bool Retry = false;
+    static bool picoPrev = false;
+
+    bool picoNow = digitalRead(PICO_FEEDBACK);
+    if (picoNow && !picoPrev)
+    {
+        syncHault = millis();
+    }
+    picoPrev = picoNow;
+
     Blynk.run();
 
-    // ---------- ALWAYS read UART ----------
-    while (Serial.available())
+    if (Blynk.connected())
     {
-        char c = Serial.read();
-
-        if (c == '\n')
+        if (digitalRead(PICO_FEEDBACK))
         {
-            rxBuff[idx] = '\0';
-            idx = 0;
+            if (Retry && picoNow)
+            {
+                if (millis() - syncHault > 5000)
+                {
+                    syncHault = millis();
+                    FlashStatus(0, 1);
+                    Blynk.syncVirtual(V2, V1, V0);
+                    Retry = false;
+                }
+            }
+            else
+            {
+                // ---------- ALWAYS read UART ----------
+                while (Serial.available())
+                {
+                    char c = Serial.read();
 
-            float units = atof(rxBuff);
+                    if (c == '\n')
+                    {
+                        rxBuff[idx] = '\0';
+                        idx = 0;
 
-            // Bill Callculation
-            float bill =
-                units >= 301 ? units * 12.83 : units <= 100 ? units * 4.43
-                                                            : units * 9.64;
+                        float units = atof(rxBuff);
 
-            char totalBill[32];
-            snprintf(totalBill, sizeof(totalBill), "%.2f", bill);
+                        // Bill Callculation
+                        float bill =
+                            units >= 301 ? units * 12.83 : units <= 100 ? units * 4.43
+                                                                        : units * 9.64;
 
-            Blynk.virtualWrite(V2, units);
-            Blynk.virtualWrite(V3, totalBill);
+                        char totalBill[32];
+                        snprintf(totalBill, sizeof(totalBill), "%.2f", bill);
+                        if (atof(totalBill) != prev_bill)
+                        {
+                            prev_bill = atof(totalBill);
+                            Blynk.virtualWrite(V3, totalBill);
+                        }
+                        if (units != prev_unit)
+                        {
+                            prev_unit = units;
+                            Blynk.virtualWrite(V2, units);
+                        }
+                    }
+                    else if (idx < sizeof(rxBuff) - 1)
+                    {
+                        rxBuff[idx++] = c;
+                    }
+                }
+            }
         }
-        else if (idx < sizeof(rxBuff) - 1)
+        else
         {
-            rxBuff[idx++] = c;
+            Retry = true;
+            FlashStatus(500);
         }
     }
 
@@ -97,14 +161,14 @@ void loop()
     if (!Blynk.connected())
     {
         unsigned long now = millis();
-
+        Retry = true;
         if (now - lastReconnectAttempt > 5000)
         {
             lastReconnectAttempt = now;
 
-            digitalWrite(UART_FLAG, 0);
+            FlashStatus(50);
             idx = 0; // buffer index clear
-            digitalWrite(LED_BUILTIN, 1);
+            digitalWrite(BLYNK_FLAG, 0);
             Blynk.connect();
         }
     }
